ce3e55d44e9488df683bb5a6a96fa58bfa5ef2dd
Felipe <feliperocha.dev27@gmail.com>
2025-02-05 22:10:31 -0300

    added update in the meals table

--- instructions.md
+++ instructions.md
@@ -8,10 +8,9 @@
     - Descrição
     - Data e Hora
     - Está dentro ou não da dieta
-[] Validar existência de cookies
 - Deve ser possível editar uma refeição, podendo alterar todos os dados acima
 - Deve ser possível apagar uma refeição
-- Deve ser possível listar todas as refeições de um usuário
+[] Deve ser possível listar todas as refeições de um usuário
 - Deve ser possível visualizar uma única refeição
 - Deve ser possível recuperar as métricas de um usuário
     - Quantidade total de refeições registradas
--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -6,24 +6,53 @@ import { randomUUID } from 'crypto'
 export async function mealRoute(app: FastifyInstance) {
   app.post('/', async (request, reply) => {
     const createNewMealBodySchema = z.object({
+      userId: z.string(),
       name: z.string(),
       description: z.string(),
-      date: z.coerce.date(),
+      date: z.string(),
       isOnDiet: z.boolean(),
     })
 
-    const { name, description, date, isOnDiet } = createNewMealBodySchema.parse(
-      request.body,
-    )
+    const { userId, name, description, date, isOnDiet } =
+      createNewMealBodySchema.parse(request.body)
 
     await knex('meals').insert({
       id: randomUUID(),
+      user_id: userId,
       name,
       description,
       date,
-      isOnDiet,
+      is_on_diet: isOnDiet,
     })
 
     reply.status(201).send()
+  })
+
+  app.put('/:mealId', async (request, reply) => {
+    const paramsSchema = z.object({ mealId: z.string().uuid() })
+
+    // resgatando ID da requisição
+    const { mealId } = paramsSchema.parse(request.params)
+
+    const updateMealBodySchema = z.object({
+      userId: z.string(),
+      name: z.string(),
+      description: z.string(),
+      date: z.string(),
+      isOnDiet: z.boolean(),
+    })
+
+    const { userId, name, description, date, isOnDiet } =
+      updateMealBodySchema.parse(request.body)
+
+    await knex('meals').where({ id: mealId }).update({
+      user_id: userId,
+      name,
+      description,
+      date,
+      is_on_diet: isOnDiet,
+    })
+
+    reply.status(204).send()
   })
 }
